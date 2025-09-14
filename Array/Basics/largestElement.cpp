89c2640f76ff1b24f15a5662de96bb53939bf9bc
PrikshitBuilds <prikshitbuilds@gmail.com>
2025-09-14 09:40:59 +0530

    Create largestElement.cpp

---
+++ Array/Basics/largestElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int largest = arr[0];
+        for(int i=0;i<n;i++){
+            if(largest<arr[i]){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+};
